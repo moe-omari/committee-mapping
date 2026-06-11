187af7d52d2c35584b7892e8d74fe7adf6bdf0e7
unknown <moee.omari@gmail.com>
2026-06-11 10:06:58 +0300

    remove content from the main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
-import { loadSites } from "./lib/committee-data";
-import { SiteDirectory } from "./ui/site-directory";
+import Image from "next/image";
 
 export const dynamic = "force-dynamic";
 
-export default async function Home() {
-  const sites = await loadSites();
-
-  return <SiteDirectory sites={sites} />;
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-[#1b1464] px-6">
+      <Image
+        src="/logo-acted-blanc.png"
+        alt="ACTED"
+        width={240}
+        height={90}
+        className="h-auto w-40 sm:w-52 md:w-60"
+        priority
+      />
+    </main>
+  );
 }
